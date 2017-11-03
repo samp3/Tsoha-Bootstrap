66caa88dc46d1448accb0baf23c4ca97bfc360dc
unknown <samuli.ahlqvist@helsinki.fi>
2017-11-03 21:46:24 +0200

    project folder vaihdettu

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="spmahlqv"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="pokemon-tietokanta"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
