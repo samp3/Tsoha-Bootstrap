2cacb7d65f5216bf48cd5a542f6c6bfe5ef89e8d
unknown <samuli.ahlqvist@helsinki.fi>
2017-11-10 18:25:11 +0200

    cotroller and routes

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,29 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  echo 'Tämä on etusivu!';
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
+    public static function sandbox() {
         View::make('helloworld.html');
-    
     }
-  }
+
+    public static function pokemon_list() {
+        View::make('suunnitelmat/pokemon_list.html');
+    }
+
+    public static function pokemon_show() {
+        View::make('suunnitelmat/pokemon_show.html');
+    }
+    public static function pokemon_show_muokkaus() {
+        View::make('suunnitelmat/pokemon_show_muokkaus.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,22 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+$routes->get('/pokemon', function() {
+    HelloWorldController::pokemon_list();
+});
+$routes->get('/pokemon/1', function() {
+    HelloWorldController::pokemon_show();
+});
+$routes->get('/pokemon/1/muokkaus', function() {
+    HelloWorldController::pokemon_show_muokkaus();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
