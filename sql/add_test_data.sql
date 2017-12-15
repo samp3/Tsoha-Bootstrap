63aec68251d5a11e9f9c4c137332ca304914345c
unknown <samuli.ahlqvist@helsinki.fi>
2017-12-15 20:14:36 +0200

    juuja

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -2,7 +2,7 @@
 INSERT INTO Kayttaja (nimi, salasana, yllapitaja) VALUES ('ylla','pitaja', true);
 INSERT INTO Kayttaja (nimi, salasana) VALUES ('sampe','sampe');
 INSERT INTO Kayttaja (nimi, salasana) VALUES ('milka','milka');
-INSERT INTO Pokemon (nimi, jarjestysnumero, tyyppi, seuraavamuoto) VALUES ('Bulbasaur', 1, 'ruoho', 'Ivysaur');
-INSERT INTO Pokemon (nimi, jarjestysnumero, tyyppi, edellinenmuoto, seuraavamuoto) VALUES ('Pikachu', 25, 'sähko', 'Pichu', 'Raichu');
-INSERT INTO KayttajaPokemon(kayttaja_nimi, pokemon_id, lempinimi, kaappauspvm, cp, iv) VALUES ('ylla', 25, 'seppo', date '2017-12-01', 500, 80);
-INSERT INTO KayttajaPokemon(kayttaja_nimi, pokemon_id, lempinimi, kaappauspvm, cp, iv) VALUES ('ylla', 25, 'yrjänä', date '2017-12-01', 450, 70);
+INSERT INTO Pokemon (id, nimi, jarjestysnumero, tyyppi, seuraavamuoto) VALUES (100000, 'Bulbasaur', 1, 'ruoho', 'Ivysaur');
+INSERT INTO Pokemon (id, nimi, jarjestysnumero, tyyppi, edellinenmuoto, seuraavamuoto) VALUES (100001, 'Pikachu', 25, 'sähko', 'Pichu', 'Raichu');
+INSERT INTO KayttajaPokemon(kayttaja_nimi, pokemon_id, lempinimi, kaappauspvm, cp, iv) VALUES ('ylla', 100000, 'seppo', date '2017-12-01', 500, 80);
+INSERT INTO KayttajaPokemon(kayttaja_nimi, pokemon_id, lempinimi, kaappauspvm, cp, iv) VALUES ('ylla', 100000, 'yrjänä', date '2017-12-01', 450, 70);
